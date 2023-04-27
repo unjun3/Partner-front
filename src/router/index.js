94ad9e9bf8a8eddb0579dce4937e752a107c7143
unjun3 <102865035+unjun3@users.noreply.github.com>
2023-04-27 23:26:23 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,38 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+import {useCounterStore} from "@/stores/counter";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/login',
+      name: 'login',
+      component: () => import("../views/Login.vue")
+    },
+    {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: () => import("../views/Home.vue")
     },
+    {
+      path: '/404',
+      name: '404',
+      component: () => import("../views/404.vue")
+    }
   ]
+})
+
+router.beforeEach((to,from,next)=>{
+  //获取缓存的用户数据
+  // const store = useCounterStore()
+  // const count =  store.count;
+  if(!to.matched.length){   //没有匹配路由
+    next("/login")
+  }else{
+    next()
+  }
+
 })
 
 export default router
